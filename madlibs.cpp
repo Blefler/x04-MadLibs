b3447e6e06f603d23d4aed9d0fc210c15e3079b9
Blefler <blefler@mail.csuchico.edu>
2018-01-31 18:11:44 -0800

    Completed the code.

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,20 @@
-//Authors: 
+//Authors: Brad Lefler 
 #include <iostream>
 #include <string>
 
-int main()
-{
-
+int main() {
+std::string name;
+std::cout << "Give me a name." << std::endl;
+std::cin >> name;
+std::string adjective;
+std::cout << "Give me an adjective." << std::endl;
+std::cin >> adjective;
+int number;
+std::cout << "Give me a number." << std::endl;
+std::cin >> number;
+char letter;
+std::cout << "Give me a letter." << std::endl;
+std::cin >> letter;
+std::cout<< name << " is a very " << adjective << " human. He is made up of " << number << " % muscle." << " He always buys grade " << letter << " meat for his muscles. "<<std::endl;
   return 0;
 }
